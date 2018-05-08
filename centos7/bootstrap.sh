d95a9b4fb8c0d6dbf073684b87fafe5a14ab4574
Andrej Maya <andrej.maya@nttdata.com>
2018-05-08 15:42:05 +0200

    setup contiv openshift showcase

--- centos7/bootstrap.sh
+++ centos7/bootstrap.sh
@@ -1,5 +1,10 @@
 yum update -y
 yum install -y telnet net-tools tcpdump iptables-services
 systemctl enable iptables.service
-curl -fsSL https://get.docker.com/ | sh
-usermod -aG docker vagrant
+
+#install ansible
+sudo yum install -y epel-release
+sudo yum install -y ansible
+
+#curl -fsSL https://get.docker.com/ | sh
+#usermod -aG docker vagrant
